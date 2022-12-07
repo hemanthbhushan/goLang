0784172dff35fa34cfdc098b84757bef8a712d10
hemanthBhushan <hemanthbhushan6@gmail.com>
2022-12-07 23:23:46 +0530

    commit message

--- check.go
+++ check.go
@@ -12,5 +12,18 @@ func main() {
 	var arr []int = []int{1, 2, 3, 4, 5}
 	arr = append(arr, 23)
 
+	//slice
+
+	rangeOne := arr1[1:3]
+	rangeTwo := arr1[:4]
+	rangeThree := arr[1:4]
+	//range one [2 3]
+	//range two [1 2 3 4]
+	//range three [1 2 3 4]
+
+	fmt.Println("range one", rangeOne)
+	fmt.Println("range two", rangeTwo)
+	fmt.Println("range three", rangeThree)
+
 	fmt.Println("arr,arr1", arr, arr1)
 }
